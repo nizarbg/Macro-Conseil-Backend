eec91948a7c0675e42c8c921a5257d0c59629e24
nizarbg <bgbgnizar@gmail.com>
2025-03-23 23:55:18 +0100

    add loggs

--- src/main/java/com/example/Macro_conseil_backend/Controller/EmailController.java
+++ src/main/java/com/example/Macro_conseil_backend/Controller/EmailController.java
@@ -1,7 +1,6 @@
 package com.example.Macro_conseil_backend.Controller;
 
 import com.example.Macro_conseil_backend.Services.EmailService;
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +17,11 @@ public class EmailController {
 
     @PostMapping("/send")
     public ResponseEntity<String> sendEmail(@RequestBody Map<String, String> request) {
-        try {
-            String name = request.get("name");
-            String email = request.get("email");
-            String message = request.get("message");
+        String name = request.get("name");
+        String email = request.get("email");
+        String message = request.get("message");
 
-            emailService.sendEmail(name, email, message);
-            return ResponseEntity.ok("Email sent successfully!");
-        } catch (MessagingException e) {
-            return ResponseEntity.status(500).body("Failed to send email.");
-        }
+        emailService.sendEmail(name, email, message);
+        return ResponseEntity.ok("Email sent successfully!");
     }
 }
